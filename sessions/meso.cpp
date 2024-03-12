b117366a755b3a1848e922681a934589d5305ac0
Devoodie <devantecoley802@gmail.com>
2024-03-12 00:31:46 -0400

    TODO>> FINISH get_length().

--- sessions/meso.cpp
+++ sessions/meso.cpp
@@ -19,5 +19,23 @@ void meso::edit_session(std::string date, session ammend){
 };
 };
 
+int meso::get_length(){
+	int month1 = std::stoi(this->sessions[0].date.substr(0,1)),
+	    day1 = std::stoi(this->sessions[0].date.substr(2,3)),
+	    year1 = std::stoi(this->sessions[0].date.substr(4,5)),
+	    month2 = std::stoi(this->sessions[this->sessions.size()-1].date.substr(0,1)),
+	    day2 = std::stoi(this->sessions[this->sessions.size()-1].date.substr(2,3)),
+	    daycount;
+	for(int i = month1; i < month2; ++i){
+		switch(i){
+			case 1:
+				daycount += 31;
+			case 2:
+				year = std::stoi(this->sessions[this->sessions.size()
+
+	};
+	return daycount;
+};
+
 
 
